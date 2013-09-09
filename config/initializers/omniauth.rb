86ca8fb1001ec413ee0e8d66b4325e882e886d02
Christoph Görn <goern@b4mad.net>
2013-09-09 20:38:34 +0200

    added prod and develop FB applications

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,14 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '587786671260237', '58f9b2bfe8ba7e931c2482e85af344dc',
-    :scope => 'publish_stream,email,offline_access,manage_pages,user_birthday,read_stream'
+# seeAlso http://stackoverflow.com/questions/4532721/facebook-development-in-localhost
+
+if Rails.env == 'development' || Rails.env == 'test'
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, '587786671260237', '58f9b2bfe8ba7e931c2482e85af344dc',
+      :scope => 'publish_stream,email,offline_access,manage_pages,user_birthday,read_stream'
+  end
+else
+  # Production
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, '514621748615493', '820a9aee790f08260dc6e9b654b16195',
+      :scope => 'publish_stream,email,offline_access,manage_pages,user_birthday,read_stream'
+  end
 end
